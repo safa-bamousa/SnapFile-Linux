4865360e1e2acf8886cfff1bd417cad8fee0c74f
safa-bamousa <safabamousa62@gmail.com>
2026-05-06 09:54:33 +0100

    Revert "Merge pull request #1 from safa-bamousa/safae"
    This reverts commit 2806687e5fd3533873646c1baa100fc1263fb31b, reversing
    changes made to 6f06f1a0ff665431829ad7c84fe725fa92ff01fe.

--- src/snapfile.sh
+++ src/snapfile.sh
@@ -50,9 +50,6 @@ source "$SCRIPT_DIR/lib/commands.sh" # cmd_save(), cmd_log(), cmd_restore(), run
 # POINT D'ENTRÉE
 # ============================================================================
 
-# Capturer Ctrl+C (Interruption)
-trap 'echo ""; die 108 "Interruption par l utilisateur"' SIGINT
-
 # 1. Parser les options et récupérer COMMAND + TARGET_DIR
 parse_options "$@"
 
